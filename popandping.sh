2ef7ed21dc1b1991947e958195eaee7eb6197ab5
Bumblebee-Man <Bumblebee-Man@badfish.me>
2014-08-17 22:40:46 -0500

    Update popandping.sh
    Added how to grab ip addresses and verify by ping.

--- popandping.sh
+++ popandping.sh
@@ -5,7 +5,7 @@ IMAGE=<DEFINE_IMAGE>
 
 ## DEFINE NETWORKS
 INSIDE=$(neutron net-list | grep INSIDE | awk '{ print $2 }')
-PROD=$(neutron net-list | grep PROD1 | awk '{ print $2 }')
+PROD=$(neutron net-list | grep PROD | awk '{ print $2 }')
 PUBLIC=$(neutron net-list | grep PUBLIC | awk '{ print $2 }')
 TEST=$(neutron net-list | grep TEST | awk '{ print $2 }')
 
@@ -16,22 +16,35 @@ PUBLIC_NS=qdhcp-`echo $PUBLIC`
 TEST_NS=qdhcp-`echo $TEST`
 
 # BOOT INSTANCES
-nova boot --image $IMAGE --flavor 1 --security-groups default \
+nova boot --image $IMAGE --flavor 2 --security-groups default \
  --nic net-id=$INSIDE test-INSIDE
 
-nova boot --image $IMAGE --flavor 1 --security-groups default \
- --nic net-id=$PROD test-PROD1
+nova boot --image $IMAGE --flavor 2 --security-groups default \
+ --nic net-id=$PROD test-PROD
 
-nova boot --image $IMAGE --flavor 1 --security-groups default \
+nova boot --image $IMAGE --flavor 2 --security-groups default \
  --nic net-id=$PUBLIC test-PUBLIC
 
-nova boot --image $IMAGE --flavor 1 --security-groups default \
+nova boot --image $IMAGE --flavor 2 --security-groups default \
  --nic net-id=$TEST test-TEST
 
 
 ## GRAB IPs
-# TO BO WORKED ON
+INSIDE_IP=$(nova list | grep INSIDE | awk '{ print $12 }' | sed 's/^.*=//')
+PROD_IP=$(nova list | grep PROD1 | awk '{ print $12 }' | sed 's/^.*=//')
+PUBLIC_IP=$(nova list | grep PUBLIC | awk '{ print $12 }' | sed 's/^.*=//')
+TEST_IP=$(nova list | grep TEST | awk '{ print $12 }' | sed 's/^.*=//')
+
 
 ## VERIFY PING
-ip netns exec $INSIDE_NS ssh -oStrictHostKeyChecking=no
-# TO BE WORKED ON
+ip netns exec $INSIDE_NS ssh \
+-oStrictHostKeyChecking=no cirros@INSIDE_IP 'ping -q -c 3 google.com'
+
+ip netns exec $PROD_NS ssh \
+-oStrictHostKeyChecking=no cirros@PROD_IP 'ping -q -c 3 google.com'
+
+ip netns exec $PUBLIC_NS ssh \
+-oStrictHostKeyChecking=no cirros@PUBLIC_IP 'ping -q -c 3 google.com'
+
+ip netns exec $TEST_NS ssh \
+-oStrictHostKeyChecking=no cirros@TEST_IP 'ping -q -c 3 google.com'
