3cb0072577e4361511acb877446696867559384b
Bumblebee-Man <Bumblebee-Man@badfish.me>
2014-08-17 22:03:22 -0500

    Update popandping.sh
    Removed a couple of items not needed

--- popandping.sh
+++ popandping.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 ## What Image to use
-IMAGE=9fd36b73-bf8a-440a-ad75-f93597924e50
+IMAGE=<DEFINE_IMAGE>
 
 ## DEFINE NETWORKS
 INSIDE=$(neutron net-list | grep INSIDE | awk '{ print $2 }')
-PROD1=$(neutron net-list | grep PROD1 | awk '{ print $2 }')
-PROD2=$(neutron net-list | grep PROD2 | awk '{ print $2 }')
+PROD=$(neutron net-list | grep PROD1 | awk '{ print $2 }')
 PUBLIC=$(neutron net-list | grep PUBLIC | awk '{ print $2 }')
 TEST=$(neutron net-list | grep TEST | awk '{ print $2 }')
 
 ## DEFINE NAMESPACES
 INSIDE_NS=qdhcp-`echo $INSIDE`
-PROD1_NS=qdhcp-`echo $PROD1`
-PROD2_NS=qdhcp-`echo $PROD2`
+PROD_NS=qdhcp-`echo $PROD1`
 PUBLIC_NS=qdhcp-`echo $PUBLIC`
 TEST_NS=qdhcp-`echo $TEST`
 
@@ -22,10 +20,7 @@ nova boot --image $IMAGE --flavor 1 --security-groups default \
  --nic net-id=$INSIDE test-INSIDE
 
 nova boot --image $IMAGE --flavor 1 --security-groups default \
- --nic net-id=$PROD1 test-PROD1
-
-nova boot --image $IMAGE --flavor 1 --security-groups default \
- --nic net-id=$PROD2 test-PROD2
+ --nic net-id=$PROD test-PROD1
 
 nova boot --image $IMAGE --flavor 1 --security-groups default \
  --nic net-id=$PUBLIC test-PUBLIC
